e136b811670deacba4ae8b43c98b14e124e9433d
Facturas <eliezelapolinaris2017@gmail.com>
2026-09-22 13:22:54 -0400

    Refresh admin PWA after document editor update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C="taller-admin-v9";
+const C="taller-admin-v10";
 const ASSETS=["./","index.html","styles.css","app.js","manifest.webmanifest","icon.svg"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith("taller-admin-")&&k!==C).map(k=>caches.delete(k)))),self.clients.claim()])));
