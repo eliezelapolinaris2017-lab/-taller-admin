a806ea4de77a376778ba570d73a6fa522727af3f
Facturas <eliezelapolinaris2017@gmail.com>
2026-09-22 23:14:11 -0400

    Refresh PWA for optional supplier search URLs

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C="taller-admin-v24";
+const C="taller-admin-v25";
 const ASSETS=["./","index.html","styles.css","app.js","manifest.webmanifest","icon.svg","assets/pdf-car-watermark.png"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith("taller-admin-")&&k!==C).map(k=>caches.delete(k)))),self.clients.claim()])));
